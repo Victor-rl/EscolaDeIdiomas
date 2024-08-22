9b01e8bd278e55e665a94f83bd13917e9c3700b0
victor-rl <victorrl@hotmail.com.br>
2024-08-22 20:23:24 -0300

    Models Criados

---
+++ EscolaDeIdiomas/Models/AlunosTurmas.cs
@@ -0,0 +1,11 @@
+﻿namespace EscolaDeIdiomas.Models
+{
+    public class AlunosTurmas
+    {
+        public int AlunoId { get; set; }
+        public int TurmaId { get; set; }
+        public Aluno Aluno { get; set; }
+        public Turma Turma { get; set; }
+
+    }
+}
---
+++ EscolaDeIdiomas/Models/Turma.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EscolaDeIdiomas.Models
+{
+    public class Turma
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Idioma { get; set; }
+        [Required]
+        public string Nivel { get; set; }
+        [Required]
+        public int Numero { get; set; }
+        [Required]
+        public int AnoLetivo { get; set; }
+        public ICollection<AlunosTurmas> AlunosTurmas { get; set; }
+    }
+}
